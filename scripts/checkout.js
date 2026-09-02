607ed9172f84ee7bea557b15291c13a856018c27
Kathir-bca <kathirg.ca@gmail.com>
2026-09-02 21:55:10 +0530

    create oop version ccart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {checkoutUpdate} from './checkout/checkoutHeader.js';
+import '../data/cart-oop.js'
 
 checkoutUpdate();
 renderOrderSummary();
