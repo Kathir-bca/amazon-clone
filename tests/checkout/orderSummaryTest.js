a0ac017e777dfb178fab7871b519a97a1a0e0687
Kathir-bca <kathirg.ca@gmail.com>
2026-09-05 09:35:27 +0530

    tests product changed

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -21,11 +21,11 @@ describe('test suite: renderOrderSummary', () => {
         spyOn(localStorage, 'getItem').and.callFake(() => {
             return JSON.stringify([
                 {
-                    productId: "4e72c9a5-83f1-46bd-a027-59c314e68f20",
+                    productId: "bc2847e9-5323-403f-b7cf-57fde044a955",
                     quantity: 2,
                     deliveryOptionId: '1'
                 }, {
-                    productId: "d84b1e63-97f5-42ca-a806-31e59c724bf8",
+                    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
                     quantity: 1,
                     deliveryOptionId: '2'
                 }
@@ -40,19 +40,19 @@ describe('test suite: renderOrderSummary', () => {
     it('dispaly the cart', () => {
 
         expect(document.querySelectorAll('.js-test-cart-item-container').length).toEqual(2);
-        expect(document.querySelector('.js-test-product-quantity-4e72c9a5-83f1-46bd-a027-59c314e68f20').innerText).toContain('Quantity: 2')
-        expect(document.querySelector('.js-test-product-quantity-d84b1e63-97f5-42ca-a806-31e59c724bf8').innerText).toContain('Quantity: 1')
+        expect(document.querySelector('.js-test-product-quantity-bc2847e9-5323-403f-b7cf-57fde044a955').innerText).toContain('Quantity: 2')
+        expect(document.querySelector('.js-test-product-quantity-15b6fc6f-327a-4ec4-896f-486349e85a3d').innerText).toContain('Quantity: 1')
 
     })
 
     it('remove from cart', () => {
 
-        document.querySelector('.js-test-delete-link-d84b1e63-97f5-42ca-a806-31e59c724bf8').click();
+        document.querySelector('.js-test-delete-link-15b6fc6f-327a-4ec4-896f-486349e85a3d').click();
         expect(document.querySelectorAll('.js-test-cart-item-container').length).toEqual(1);
-        expect(document.querySelector('.js-cart-item-container-d84b1e63-97f5-42ca-a806-31e59c724bf8')).toEqual(null);
-        expect(document.querySelector('.js-cart-item-container-4e72c9a5-83f1-46bd-a027-59c314e68f20')).not.toEqual(null);
+        expect(document.querySelector('.js-cart-item-container-15b6fc6f-327a-4ec4-896f-486349e85a3d')).toEqual(null);
+        expect(document.querySelector('.js-cart-item-container-bc2847e9-5323-403f-b7cf-57fde044a955')).not.toEqual(null);
         expect(cart.length).toEqual(1);
-        expect(cart[0].productId).toEqual('4e72c9a5-83f1-46bd-a027-59c314e68f20');
+        expect(cart[0].productId).toEqual('bc2847e9-5323-403f-b7cf-57fde044a955');
 
     });
 
