4e58cf7269834f6236f42a8bc966f23001d2b3a8
Kathir-bca <kathirg.ca@gmail.com>
2026-09-06 14:59:43 +0530

    change amazon.html to index.html

--- scripts/checkout/checkoutHeader.js
+++ scripts/checkout/checkoutHeader.js
@@ -3,7 +3,7 @@ import { cart, totalCartQuantityUpdate } from "../../data/cart.js";
 
 export function checkoutUpdate() {
     let totalCartQuantity = totalCartQuantityUpdate();
-    let checkoutHTML = `Checkout (<a class="return-to-home-link" href="amazon.html">${totalCartQuantity} items</a>)`
+    let checkoutHTML = `Checkout (<a class="return-to-home-link" href="index.html">${totalCartQuantity} items</a>)`
     document.querySelector('.js-checkout-header-middle-section').innerHTML = checkoutHTML;    
 }
 
